f1bba8106bbdf9fbacd45082bc840a7630d7c11a
superCreature0723 <mosesberggren2@gmail.com>
2025-04-25 02:04:36 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./style.css";
 import reportWebVitals from "./reportWebVitals";
-import Shop from "./component/hookChapter/shop";
+import SocialNetwork from "./component/hookChapter/SocialNetwork";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Shop />
+    <SocialNetwork />
   </React.StrictMode>
 );
 
