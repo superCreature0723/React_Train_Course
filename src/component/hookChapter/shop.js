bc45ff90b4a437dacf34576ae6a593151186c60f
superCreature0723 <mosesberggren2@gmail.com>
2025-04-26 16:54:53 +0300

    fix

--- src/component/hookChapter/shop.js
+++ src/component/hookChapter/shop.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { get } from "./../../mockBackend/fetch";
+import { get } from "../../mockBackend/fetch";
 
 export default function Shop() {
   const [categories, setCategories] = useState(null);
